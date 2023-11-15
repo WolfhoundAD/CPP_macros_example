aa6d5b6eda10c9410e52616779a4bae46dc9b4ca
WolfhoundAD <Loners14@yandex.ru>
2023-11-15 21:09:10 +0300

    first commit

---
+++ Macros_example.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+/*
+Этот макрос DECLARE_TYPED_CONTAINER определяет простую структуру контейнера, специфичную для типа. 
+Обратите внимание, что это не идеальный способ создания типобезопасных контейнеров, и лучше всего использовать 
+шаблоны для этой цели. В реальном коде сложные макросы могут становиться трудными для понимания и отладки.
+*/
+#define DECLARE_TYPED_CONTAINER(ContainerType, ElementType) \
+    struct ContainerType { \
+        std::vector<ElementType> elements; \
+        \
+        void add(ElementType element) { \
+            elements.push_back(element); \
+        } \
+        \
+        void display() const { \
+            std::cout << "Contents of " #ContainerType ": "; \
+            for (const auto& element : elements) { \
+                std::cout << element << " "; \
+            } \
+            std::cout << std::endl; \
+        } \
+    };
+
+// Использование макроса для создания типобезопасного контейнера
+DECLARE_TYPED_CONTAINER(IntContainer, int)
+
+int main() {
+    IntContainer container;
+    container.add(1);
+    container.add(2);
+    container.display();
+
+    return 0;
+}
---
+++ Macros_example2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+/*
+В этом примере макрос GET_SET_PROPERTY генерирует геттеры и сеттеры для заданного свойства (поля) в классе Person.
+Хотя такой подход с использованием макросов может быть мощным средством для автоматизации создания повторяющегося кода,
+он также может усложнить отладку и понимание кода, поэтому использовать рекомендуется оккуратно.
+*/
+#define GET_SET_PROPERTY(type, name) \
+private: \
+    type name##_; \
+public: \
+    const type& get_##name() const { \
+        return name##_; \
+    } \
+    void set_##name(const type& value) { \
+        name##_ = value; \
+    }
+
+class Person {
+    GET_SET_PROPERTY(std::string, Name)
+        GET_SET_PROPERTY(int, Age)
+        GET_SET_PROPERTY(double, Salary)
+public:
+    // Конструктор
+    Person(const std::string& name, int age, double salary)
+        : Name_(name), Age_(age), Salary_(salary) {}
+
+    // Другие методы класса...
+};
+
+int main() {
+    Person person("John Doe", 25, 50000.0);
+
+    // Использование геттеров
+    std::cout << "Name: " << person.get_Name() << std::endl;
+    std::cout << "Age: " << person.get_Age() << std::endl;
+    std::cout << "Salary: " << person.get_Salary() << std::endl;
+
+    // Использование сеттеров
+    person.set_Name("Jane Doe");
+    person.set_Age(30);
+    person.set_Salary(60000.0);
+
+    // Проверка изменений
+    std::cout << "\nUpdated Information:\n";
+    std::cout << "Name: " << person.get_Name() << std::endl;
+    std::cout << "Age: " << person.get_Age() << std::endl;
+    std::cout << "Salary: " << person.get_Salary() << std::endl;
+
+    return 0;
+}
